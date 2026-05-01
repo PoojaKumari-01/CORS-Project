7f54966aa0ab144e1935ada290be79a32a40f3e6
PoojaKumari-01 <pooja.2k25gmail.com>
2026-05-01 19:51:28 +0530

    updated vercel backend link

--- Project/frontend/src/services/api.js
+++ Project/frontend/src/services/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-// const API_BASE_URL = "http://localhost:9000/api";
-
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:9000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:9000/api',
   withCredentials: true, // Include cookies for JWT
   headers: {
     "Content-Type": "application/json",
