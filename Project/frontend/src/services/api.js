b3ef3015df68dc181b9612964e6daa4eb12de6c3
PoojaKumari-01 <pooja.2k25gmail.com>
2026-05-01 19:08:38 +0530

    updated vercel backend link

--- Project/frontend/src/services/api.js
+++ Project/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:9000/api";
+const API_BASE_URL = "https://cors-project-production.up.railway.app/api";
 
 // Create axios instance with default config
 const api = axios.create({
